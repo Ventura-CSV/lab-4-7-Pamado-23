372733c8d4bc4841efcd13875989c091e92965f5
Pamado-23 <pamado1@my.vcccd.edu>
2024-06-28 12:16:58 -0700

    Lab 4-7

--- main.py
+++ main.py
@@ -6,6 +6,20 @@ def main():
     ########################################
     """
 
+    previous_value = int(input ())
+    numbers.append(previous_value)
+
+    while (True):
+        current_value = int(input ())
+
+        if current_value <= previous_value:
+            numbers.append(current_value)
+            previous_value = current_value
+        else:
+            break
+
+
+
     ########################################
     # Do not delete the return statement
     ########################################
